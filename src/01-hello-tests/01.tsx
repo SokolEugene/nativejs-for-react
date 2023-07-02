9ef63a43fad96cdb04ab0b554ebc79a25926e3e5
SokolEugene <zheksem@gmail.com>
2023-07-02 16:03:21 +0300

    first commit by Asus

---
+++ src/01-hello-tests/01.tsx
@@ -0,0 +1,18 @@
+export function sum(a:number, b:number) {
+    return a + b;
+}
+
+export function mult(a:number, b:number) {
+    return a * b;
+}
+
+sum(5, 10);
+mult(5, 10)
+
+
+
+/*
+export const sentence = 'Hello my words!'
+function splitIntoWords(sentence) {
+    return ['hello', 'my', 'words']
+}*/
